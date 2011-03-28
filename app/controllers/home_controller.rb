2dd9c5f5360c3dda69c1299a498a954ccb67d220
Jared Beck <jared@jaredbeck.com>
2011-03-28 03:57:10 -0400

    linky link link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
+
   def index
+    @links = []
+    @links << 'http://www.facebook.com/jaredowenbeck'
+    @links << 'http://github.com/jaredbeck'
+    @links << 'http://www.singlebrook.com'
   end
 
 end
