43f8b6eca353a56923cb54fdd630d8d5bb31960e
Jared Beck <jared@jaredbeck.com>
2011-03-28 03:47:17 -0400

    IR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jaredbeck::Application.config.secret_token = 'd5846635df69a275e4eeea50d72b2633c7386e8723113c096cf3db99a685d9c91d970000f45b6794342a7e6d774973768b80220ed7d3805f0da51538d7ae5d62'
