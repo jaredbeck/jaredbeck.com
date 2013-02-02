61f98d9b5323f7cdf646ce523d039425b18a9e37
Jared Beck <jared@jaredbeck.com>
2013-02-02 03:12:49 -0500

    remove portrait, add SO link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :define_links_for_header
-  
+
   def define_links_for_header
-    @links = []
-    @links << link_hash('github', 'http://github.com/jaredbeck')
-    @links << link_hash('singlebrook', 'http://www.singlebrook.com')
-    @links << link_hash('facebook', 'http://www.facebook.com/jaredowenbeck')
+    @links = [
+      { text: 'singlebrook', pre: 'works at', url: 'http://www.singlebrook.com' },
+      { text: 'github', pre: 'codes on', url: 'https://github.com/jaredbeck'},
+      { text: 'stackoverflow', pre: 'thanks heaven for', url: 'http://stackoverflow.com/users/567762/jared-beck'},
+      { text: 'facebook', pre: 'still not sure about', url: 'https://www.facebook.com/jaredowenbeck' }
+    ]
   end
-
-private
-
-  def link_hash(t,u)
-    { :text => t, :url => u }
-  end
-
 end
