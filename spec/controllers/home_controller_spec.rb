b4839b236bc6f6f774ba01a4964d4c11e1be4bbf
Jared Beck <jared@jaredbeck.com>
2013-02-02 02:24:10 -0500

    finish converting tests to specs

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe HomeController do
+  describe '#index' do
+    it 'succeeds' do
+      get :index
+      response.should be_success
+      response.should render_template :index
+    end
+  end
+end
