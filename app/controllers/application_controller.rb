a8192bd5342310527beaa34e981485c3ef14ff11
Jared Beck <jared@jaredbeck.com>
2014-10-16 13:30:09 -0400

    Remove singlebrook, play with design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :define_links_for_header
+  before_action :define_links_for_header
 
   def define_links_for_header
     @links = [
-      { text: 'singlebrook', url: 'http://www.singlebrook.com' },
       { text: 'github', url: 'https://github.com/jaredbeck'},
       { text: 'stackoverflow', url: 'http://stackoverflow.com/users/567762/jared-beck'},
       { text: 'facebook', url: 'https://www.facebook.com/jaredowenbeck' },
